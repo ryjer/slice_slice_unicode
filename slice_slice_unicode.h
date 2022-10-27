50a4e13dbc8a8c6f44db814c7b1f53d1d9ea1489
jiakun <jiakun_wang@yeah.net>
2022-10-27 12:55:25 +0000

    +字符串切片遍历搜索 完成测试

--- slice_slice_unicode.h
+++ slice_slice_unicode.h
@@ -10,8 +10,9 @@
         uint32_t 容量;
         unicode切片 * 数据区; //指向unicode切片数组的数据指针
     } unicode切片动态数组;
-
+    
     // 提供以下操作函数
+
     void unicode切片动态数组_初始化(unicode切片动态数组 * 数组指针, int 初始容量);
     void unicode切片动态数组_清空(unicode切片动态数组 * 数组指针);
     void unicode切片动态数组_重置(unicode切片动态数组 * 数组指针);
@@ -19,5 +20,7 @@
     void unicode切片动态数组_追加(unicode切片动态数组 * 数组指针, unicode切片 待追加切片);
     void unicode切片动态数组_文件打印(unicode切片动态数组 * 数组指针, FILE * 输出文件);
     void unicode切片动态数组_文件序列化打印(unicode切片动态数组 * 数组指针, FILE * 输出文件);
-    
+    // 以下为纯函数
+
+    int unicode切片动态数组_遍历搜索(unicode切片动态数组 * 被搜索数组, unicode切片 目标切片);
 #endif
